f1f14bbc8b45c2f1c090ec8a1d83b76c26c8f157
tlightk <klayxi@gmail.com>
2020-10-20 01:21:38 +0900

    created simple html and updated client.js file

--- client/client.js
+++ client/client.js
@@ -3,4 +3,9 @@ window.onload = () => {
     paragraphElement.setAttribute('id', 'introduction')
     paragraphElement.innerText = 'Hello! Welcome to my first solo API project as a student at Code Chrysalis!';
     document.body.appendChild(paragraphElement);
+
+    const nextParagraphElement = document.createElement('p');
+    nextParagraphElement.setAttribute('id', 'introduction')
+    nextParagraphElement.innerHTML = '<a href=https://github.com/tlightk/apisoloproject>Click here to see my project!</a>';
+    document.body.appendChild(nextParagraphElement);
 };
