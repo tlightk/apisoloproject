3ec74443fa266abfe73841806f7f8c4bb91a0210
tlightk <klayxi@gmail.com>
2020-10-19 13:50:10 +0900

    added knexfile.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+const knexfile = require("./knexfile");
+const knex = require('knex')({
+    client: 'postgres',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'wordpass1',
+      database : 'solo_api'
+    }
+  });
+
+const users = knex.select().table("users").
+then((rows) => {
+    console.log(rows);
+});
+
+console.log(users);
+
 const express = require("express");
 
 const app = express();
